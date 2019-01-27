8cf204035cd971d3d46d49a6a375cec2cc56040f
Kleber Ayala <kleber.ayala@gmail.com>
2019-01-27 15:38:37 -0500

    initial commit

---
+++ api/src/main/java/org/ecjug/hackday/api/EventService.java
@@ -0,0 +1,18 @@
+package org.ecjug.hackday.api;
+
+import org.ecjug.hackday.domain.model.Event;
+import org.ecjug.hackday.domain.model.Member;
+
+import java.util.List;
+
+/**
+ * @author Kleber Ayala
+ */
+public interface EventService {
+
+    Event add(Event event);
+
+    List<Event> list();
+
+    void addMemberToEvent(String eventId, Member member);
+}
